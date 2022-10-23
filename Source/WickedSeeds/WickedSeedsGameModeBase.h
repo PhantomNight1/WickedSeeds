dcfa6e57a77882cfc19bcbe57cfc9ead1f169af8
Mario Egocheaga <43833659+Mario-Egocheaga@users.noreply.github.com>
2022-10-22 22:27:25 -0400

    Intital Commit

---
+++ Source/WickedSeeds/WickedSeedsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WickedSeedsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WICKEDSEEDS_API AWickedSeedsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
